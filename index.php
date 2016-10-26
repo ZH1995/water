79e65aedcff59a366ed8289b71b232912594a61f
ZH1995 <rwzsjfm@qq.com>
2016-10-26 16:17:07 +0800

    add time show

--- index.php
+++ index.php
@@ -7,11 +7,24 @@
 	<body>
 		<center>
 			<h1>抢票网站</h1>
+
+			<div id="time">
+
+			</div>
+
 			<div class='button'>
 				<button id="rush">立即抢票</button>
 			</div>
 		<center>
 	</body>
+
+	<script type="text/javascript">
+		var dateTime = new Date();
+		var hh = dateTime.getHours();
+		var mm = dateTime.getMinutes();
+		var ss = dateTime.getSeconds();
+		document.getElementById("time").innerHTML = hh + ":" + mm + ":" + ss;
+	</script>
 
 	<script type='text/javascript'>
 		$(function() {
@@ -20,4 +33,5 @@
 			});
 		});
 	</script>
+
 </html>

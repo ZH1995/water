1b2bf4fea576b3dd73c4365fd775926d79e2b164
ZH1995 <rwzsjfm@qq.com>
2016-10-26 15:59:39 +0800

    modify index.php

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
-<?php
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<title>抢票网站</title>
+		<script type='text/javascript' src='http://cdn.static.runoob.com/libs/jquery/1.10.2/jquery.min.js'></script>
+	</head>
+	<body>
+		<h1>抢票网站</h1>
+		<div class='button'>
+			<button id="rush">立即抢票</button>
+		</div>
+	</body>
 
-session_start();
-if (!isset($_SESSION['admin']) || $_SESSION['admin'] == '') {
-	header('./web/login.html');
-	exit();
-}
-?>
+
+
+	<script type='text/javascript'>
+		$(function() {
+			$("#rush").click(function() {
+				console.info("抢票成功！");
+			});
+		});
+	</script>
+</html>

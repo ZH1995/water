90ebed336e38821b9a29ede9ba65505aeb9f3697
ZH1995 <rwzsjfm@qq.com>
2016-10-26 16:22:45 +0800

    add time show

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 		<title>抢票网站</title>
 		<script type='text/javascript' src='http://cdn.static.runoob.com/libs/jquery/1.10.2/jquery.min.js'></script>
 	</head>
-	<body>
+	<body onload="showTime()">
 		<center>
 			<h1>抢票网站</h1>
 
+			<h3>下一波抢票时间为 2016/10/30 19:00:00</h3>
 			<div id="time">
-
 			</div>
 
 			<div class='button'>
@@ -19,11 +19,23 @@
 	</body>
 
 	<script type="text/javascript">
-		var dateTime = new Date();
-		var hh = dateTime.getHours();
-		var mm = dateTime.getMinutes();
-		var ss = dateTime.getSeconds();
-		document.getElementById("time").innerHTML = hh + ":" + mm + ":" + ss;
+		function showTime() {
+			var dateTime = new Date();
+			var hh = dateTime.getHours();
+			var mm = dateTime.getMinutes();
+			var ss = dateTime.getSeconds();
+
+			mm = extra(mm);
+			ss = extra(ss);
+			document.getElementById("time").innerHTML = "当前时间是 "hh + ":" + mm + ":" + ss;
+			setTimeout("showTime()",1000);
+		}
+		function extra(num) {
+			if (num < 10) {
+				return "0" + x;
+			}
+			return x;
+		}
 	</script>
 
 	<script type='text/javascript'>

ad6cd6693be90223b54edb4f947d232452278ef2
ZH1995 <rwzsjfm@qq.com>
2016-10-27 10:08:27 +0800

    add error page

---
+++ web/error.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">  
+		<title>抢票网站</title>
+		<script type='text/javascript' src='http://cdn.static.runoob.com/libs/jquery/1.10.2/jquery.min.js'></script>
+		
+	</head>
+
+	<script type="text/javascript">
+
+	</script>  
+
+	<body>
+		<center>
+			
+			<h1>这是错误页面</h1>
+		<center>
+	</body>
+</html>
+

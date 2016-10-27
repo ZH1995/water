c8c4dd22f57e6253ffe539f2acc7a2bd342df28f
ZH1995 <rwzsjfm@qq.com>
2016-10-27 10:11:30 +0800

    debug login

--- core/login.php
+++ core/login.php
@@ -1,10 +1,11 @@
 <?php
 
-$email    = $_POST["email"];
-$password = $_POST["password"];
+$email    = trim($_POST["email"]);
+$password = trim($_POST["password"]);
 
 if (empty($email) || empty($password)) {
-	header("../web/error.php");
+	header("Location: ../web/error.php");
+	exit;
 } 
 
 // 用户验权
